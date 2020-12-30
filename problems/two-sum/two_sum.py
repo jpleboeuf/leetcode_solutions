d51c578004677b4c3f56127b41c579816a3c0824
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2020-12-30 19:53:09 +0100

    Solve problem 1. "Two Sum" (cont.)
    Extracted the main code to a snake_case-named function
     - I feel better.

--- problems/two-sum/two_sum.py
+++ problems/two-sum/two_sum.py
@@ -1,24 +1,28 @@
 from typing import List
 
 
+def two_sum(nums: List[int], target: int) -> List[int]:
+    r: int = []
+    i = 0
+    for ni in nums:
+        j = 0
+        for nj in nums:
+            if i == j:
+                continue
+            if ni + nj == target:
+                r = [i, j]
+                break
+            j += 1
+        else:
+            i += 1
+            continue
+        break
+    return r
+
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        twoSum_rv: int = []
-        i = 0
-        for ni in nums:
-            j = 0
-            for nj in nums:
-                if i == j:
-                    continue
-                if ni + nj == target:
-                    twoSum_rv = [i, j]
-                    break
-                j += 1
-            else:
-                i += 1
-                continue
-            break
-        return twoSum_rv
+        return two_sum(nums, target)
 
 
 nums = [2, 7, 11, 15]
