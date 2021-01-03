291419d163204fedc1f97ca862f1f67142ebef5a
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2021-01-03 23:21:17 +0100

    Solve problem 2. "Add Two Numbers" (cont.)
    Added type hint to the singly-linked list initializator.

--- problems/add-two-numbers/add_two_numbers.py
+++ problems/add-two-numbers/add_two_numbers.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from typing import List
 
 from pprint import pprint
@@ -6,7 +7,7 @@ from pprint import pprint
 # Definition for singly-linked list.
 class ListNode:
     
-    def __init__(self, val=0, next=None):
+    def __init__(self, val:int=0, next:ListNode=None):
         self.val = val
         self.next = next
 
