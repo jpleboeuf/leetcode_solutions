afbfd787c26189e37e89f5c9aa665ff7f1ecf476
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2021-01-23 20:45:10 +0100

    Solve problem 509. "Fibonacci Number" (cont.)
    For the solutions using function attributes
     (the solution using "Recursion"
     and the solution using memoization & recursion),
     moved the manipulation of function attributes outside of functions
     in order to save one test each time these functions are called.

--- problems/fibonacci-number/fibonacci_number_hybrid.py
+++ problems/fibonacci-number/fibonacci_number_hybrid.py
@@ -3,13 +3,6 @@ from typing import Final
 
 
 def fib(n: int) -> int:
-    # Using function attributes to emulate static variables:
-    if not hasattr(fib, "f"):
-        # typing.Final not supported by pylint at this time
-        #  https://github.com/PyCQA/pylint/issues/3197
-        fib.f0: Final[int] = 0  # pylint: disable=unsubscriptable-object
-        fib.f1: Final[int] = 1  # pylint: disable=unsubscriptable-object
-        fib.f: List[int] = [fib.f0, fib.f1]
     if n == 0:
         return fib.f0
     elif n == 1:
@@ -17,6 +10,12 @@ def fib(n: int) -> int:
     if n >= len(fib.f):
         fib.f.append(fib(n - 1) + fib(n - 2))
     return fib.f[n]
+# Using function attributes to emulate static variables:
+# typing.Final not supported by pylint at this time
+#  https://github.com/PyCQA/pylint/issues/3197
+fib.f0: Final[int] = 0  # pylint: disable=unsubscriptable-object
+fib.f1: Final[int] = 1  # pylint: disable=unsubscriptable-object
+fib.f: List[int] = [fib.f0, fib.f1]
 
 
 class Solution:
--- problems/fibonacci-number/fibonacci_number_rec.py
+++ problems/fibonacci-number/fibonacci_number_rec.py
@@ -3,17 +3,16 @@ from typing import Final
 
 
 def fib(n: int) -> int:
-    # Using function attributes to emulate static variables:
-    if not (hasattr(fib, "f0")):
-        # typing.Final not supported by pylint at this time
-        #  https://github.com/PyCQA/pylint/issues/3197
-        fib.f0: Final[int] = 0  # pylint: disable=unsubscriptable-object
-        fib.f1: Final[int] = 1  # pylint: disable=unsubscriptable-object
     if n == 0:
         return fib.f0
     elif n == 1:
         return fib.f1
     return fib(n - 1) + fib(n - 2)
+# Using function attributes to emulate static variables:
+# typing.Final not supported by pylint at this time
+#  https://github.com/PyCQA/pylint/issues/3197
+fib.f0: Final[int] = 0  # pylint: disable=unsubscriptable-object
+fib.f1: Final[int] = 1  # pylint: disable=unsubscriptable-object
 
 
 class Solution:
