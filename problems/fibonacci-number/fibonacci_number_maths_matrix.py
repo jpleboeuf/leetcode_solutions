d8b71c7622e4cf4d667354fc84da86d2fb252ca3
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2021-01-27 23:48:04 +0100

    Solve problem 509. "Fibonacci Number" (cont.)
    Implemented a "Math" solution,
     using "Matrix Exponentiation".

--- problems/fibonacci-number/fibonacci_number_maths_matrix.py
+++ problems/fibonacci-number/fibonacci_number_maths_matrix.py
@@ -1,23 +1,66 @@
 from typing import Final
 
 
-def fib(n: int) -> int:
-    """Return the Fibonacci number for n.
-    This version uses Binet's formula,
-     whose value for n is computed by rounding.
-    Binet's formula:
-     Fₙ = ( ɸⁿ - ψⁿ ) / √5
-      where:
-       ɸ = ( 1 + √5 ) / 2  (golden ratio)
-       ψ = ( 1 - √5 ) / 2
-    Since | ψⁿ / √5 | < ½ for all n ≥ 0,
-     Fₙ is the closest integer to ɸⁿ / √5.
-    Therefore Fₙ = [ ɸⁿ / √5 ].
+def matrix22_pow(M: list, n: int) -> list:
     """
-    # typing.Final not supported by pylint at this time
-    #  https://github.com/PyCQA/pylint/issues/3197
-    phi: Final[float] = ( 1 + 5 ** 0.5 ) / 2  # pylint: disable=unsubscriptable-object
-    return int( round( phi ** n / 5 ** 0.5 ) )
+    Return a new 2×2 matrix,
+     result of the exponentiation of the 2×2 matrix M by n. 
+    """
+    if n <= 1:
+        return M
+    Msq = matrix22_mul(M, M)
+    if n % 2 == 0:  # n is even
+        return matrix22_pow(Msq, n // 2)
+    else:           # n is odd
+        return matrix22_mul(M, matrix22_pow(Msq, (n - 1) // 2))
+
+def matrix22_mul(M1: list, M2: list) -> list:
+    """
+    Return a new 2×2 matrix,
+     result of the multiplication of the two 2×2 matrices M1 and M2.
+    """
+    mm_00 = M1[0][0] * M2[0][0] + M1[0][1] * M2[1][0]
+    mm_01 = M1[0][0] * M2[0][1] + M1[0][1] * M2[1][1]
+    mm_10 = M1[1][0] * M2[0][0] + M1[1][1] * M2[1][0]
+    mm_11 = M1[1][0] * M2[0][1] + M1[1][1] * M2[1][1]
+    return [
+            [ mm_00, mm_01 ],
+            [ mm_10, mm_11 ],
+        ]
+
+
+def fib(n: int) -> int:
+    # pylint: disable=anomalous-backslash-in-string
+    """Return the Fibonacci number for n.
+    This version uses the matrix form of the Fibonacci sequence.
+    We have the following recurrence relation
+     that describes the Fibonacci sequence:
+     / Fₖ₊₂ \ = / 1 1 \ / Fₖ₊₁ \ ,
+     \ Fₖ₊₁ /   \ 1 0 / \ Fₖ   /
+     alternatively denoted F⃗ₖ₊₁ = A F⃗ₖ.
+    Then:
+     / Fₙ₊₁ \ = / 1 1 \ⁿ / F₁ \ ,
+     \ Fₙ   /   \ 1 0 /  \ F₀ /
+     also denoted F⃗ₙ = Aⁿ F⃗₀.
+    """
+    # pylint: enable=anomalous-backslash-in-string
+    F0 = [
+            [1],
+            [0],
+        ]
+    A = [
+            [1, 1],
+            [1, 0],
+        ]
+    if n <= 1:
+        return F0[n][-1]
+    Aexpnmin1 = matrix22_pow(A, n - 1)
+    """
+    Fn = Aexpnmin1[0][0] * F0[0][0] + Aexpnmin1[0][-1] * F0[1][-1]
+    Since F0[0][0] = 1 and F0[1][-1] = 0,
+     we have Fn = Aexpnmin1[0][0].
+    """
+    return Aexpnmin1[0][0]
 
 
 class Solution:
