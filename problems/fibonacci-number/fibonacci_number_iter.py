8da363cfd837b6c8360a7851471a9aeadcc5804c
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2021-02-02 22:28:58 +0100

    Solve problem 509. "Fibonacci Number" (cont.)
    Modified the "Iterative Top-Down" solution
     so that it looks more like the other solutions
     (using function attributes to store F₀ and F₁).

--- problems/fibonacci-number/fibonacci_number_iter.py
+++ problems/fibonacci-number/fibonacci_number_iter.py
@@ -1,12 +1,15 @@
 from typing import List
+from typing import Final
 
 
 def fib(n: int) -> int:
-    if n <= 1:
-        return n
+    if n == 0:
+        return fib.f0
+    elif n == 1:
+        return fib.f1
     k: int = 2
-    f_kmin2: int = 0
-    f_kmin1: int = 1
+    f_kmin2: int = fib.f0
+    f_kmin1: int = fib.f1
     f_k: int
     while k <= n:
         f_k = f_kmin1 + f_kmin2
@@ -14,6 +17,11 @@ def fib(n: int) -> int:
         f_kmin2 = f_kmin1
         f_kmin1 = f_k
     return f_k
+# Using function attributes to emulate static variables:
+# typing.Final not supported by pylint at this time
+#  https://github.com/PyCQA/pylint/issues/3197
+fib.f0: Final[int] = 0  # pylint: disable=unsubscriptable-object
+fib.f1: Final[int] = 1  # pylint: disable=unsubscriptable-object
 
 
 class Solution:
