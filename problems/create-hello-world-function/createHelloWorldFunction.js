633dc5bee85df15437fef24191108fbfd78f460a
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2024-08-21 14:36:37 +0200

    Solve problem 2667. "Create Hello Word Function": add examples as tests

--- problems/create-hello-world-function/createHelloWorldFunction.js
+++ problems/create-hello-world-function/createHelloWorldFunction.js
@@ -8,7 +8,12 @@ var createHelloWorld = function() {
   }
 };
 
-/**
-* const f = createHelloWorld();
-* f(); // "Hello World"
-*/
+const assert = require('node:assert').strict;
+
+const f1 = createHelloWorld();
+const args1 = [];
+assert.equal(f1(args1), "Hello World");
+
+const f2 = createHelloWorld();
+const args2 = [{}, null, 42];
+assert.equal(f2(args2), "Hello World");
