564ad46c379b1a20f133b4264f5e9619c5c815b3
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2024-08-21 18:16:32 +0200

    Solve problem 2665. "Counter II": add examples as tests

--- problems/counter-ii/counterII.js
+++ problems/counter-ii/counterII.js
@@ -6,9 +6,16 @@ var createCounter = function(init) {
     
 };
 
-/**
- * const counter = createCounter(5)
- * counter.increment(); // 6
- * counter.reset(); // 5
- * counter.decrement(); // 4
- */
+const assert = require('node:assert').strict;
+
+const counter1 = createCounter(5);
+assert.equal(counter1.increment(), 6);
+assert.equal(counter1.reset(), 5);
+assert.equal(counter1.decrement(), 4);
+
+const counter2 = createCounter(0);
+assert.equal(counter2.increment(), 1);
+assert.equal(counter2.increment(), 2);
+assert.equal(counter2.decrement(), 1);
+assert.equal(counter2.reset(), 0);
+assert.equal(counter2.reset(), 0);
