db3acc979e6123b3a22a2f7d7321e49e287d7e3e
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2024-08-21 16:03:11 +0200

    Solve problem 2620. "Counter" (new)

--- problems/counter/counter.js
+++ problems/counter/counter.js
@@ -2,11 +2,7 @@
  * @param {number} n
  * @return {Function} counter
  */
-var createCounter = function(n) {
-  return function() {
-      return n++;
-  };
-};
+const createCounter = (n) => () => n++;
 
 const assert = require('node:assert').strict;
 
