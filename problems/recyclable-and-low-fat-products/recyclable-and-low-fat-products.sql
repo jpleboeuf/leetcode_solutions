639bed9b00a9ab68b5639c55d8e142e19fa643df
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2024-08-26 19:29:26 +0200

    Solve problem 1757. "Recyclable and Low Fat Products": fix SQL schema

--- problems/recyclable-and-low-fat-products/recyclable-and-low-fat-products.sql
+++ problems/recyclable-and-low-fat-products/recyclable-and-low-fat-products.sql
@@ -1,9 +1,9 @@
-Create table If Not Exists Products (product_id int, low_fats ENUM('Y', 'N'), recyclable ENUM('Y','N'))
-Truncate table Products
-insert into Products (product_id, low_fats, recyclable) values ('0', 'Y', 'N')
-insert into Products (product_id, low_fats, recyclable) values ('1', 'Y', 'Y')
-insert into Products (product_id, low_fats, recyclable) values ('2', 'N', 'Y')
-insert into Products (product_id, low_fats, recyclable) values ('3', 'Y', 'Y')
-insert into Products (product_id, low_fats, recyclable) values ('4', 'N', 'N')
+CREATE TABLE IF NOT EXISTS Products (product_id INT, low_fats ENUM('Y', 'N'), recyclable ENUM('Y','N'));
+TRUNCATE TABLE Products;
+INSERT INTO Products (product_id, low_fats, recyclable) VALUES ('0', 'Y', 'N');
+INSERT INTO Products (product_id, low_fats, recyclable) VALUES ('1', 'Y', 'Y');
+INSERT INTO Products (product_id, low_fats, recyclable) VALUES ('2', 'N', 'Y');
+INSERT INTO Products (product_id, low_fats, recyclable) VALUES ('3', 'Y', 'Y');
+INSERT INTO Products (product_id, low_fats, recyclable) VALUES ('4', 'N', 'N');
 
 # Write your MySQL query statement below
