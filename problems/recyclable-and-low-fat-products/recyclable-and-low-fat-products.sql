72ee6df9b6892ae17236d0c4be190ce8dad4bcf0
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2024-08-27 16:30:17 +0200

    Solve problem 1757. "Recyclable and Low Fat Products": improve SQL code

--- problems/recyclable-and-low-fat-products/recyclable-and-low-fat-products.sql
+++ problems/recyclable-and-low-fat-products/recyclable-and-low-fat-products.sql
@@ -1,12 +1,21 @@
 CREATE TABLE IF NOT EXISTS Products (product_id INT, low_fats ENUM('Y', 'N'), recyclable ENUM('Y','N'));
 TRUNCATE TABLE Products;
-INSERT INTO Products (product_id, low_fats, recyclable) VALUES ('0', 'Y', 'N');
-INSERT INTO Products (product_id, low_fats, recyclable) VALUES ('1', 'Y', 'Y');
-INSERT INTO Products (product_id, low_fats, recyclable) VALUES ('2', 'N', 'Y');
-INSERT INTO Products (product_id, low_fats, recyclable) VALUES ('3', 'Y', 'Y');
-INSERT INTO Products (product_id, low_fats, recyclable) VALUES ('4', 'N', 'N');
+INSERT INTO Products (product_id, low_fats, recyclable) VALUES
+  ROW('0', 'Y', 'N'),
+  ROW('1', 'Y', 'Y'),
+  ROW('2', 'N', 'Y'),
+  ROW('3', 'Y', 'Y'),
+  ROW('4', 'N', 'N');
 
-# Write your MySQL query statement below
+# Example 1:
+CREATE TABLE IF NOT EXISTS Example1 (product_id INT);
+TRUNCATE TABLE Example1;
+INSERT INTO Example1 (product_id) VALUES
+  ROW(1),
+  ROW(3);
+SELECT * FROM Example1;
+
+# Solution:
 DROP TABLE IF EXISTS Solution;
 CREATE TABLE Solution AS (
   SELECT p.product_id
@@ -15,10 +24,5 @@ CREATE TABLE Solution AS (
 );
 SELECT * FROM Solution;
 
-# Example 1:
-CREATE TABLE IF NOT EXISTS Example1 (product_id INT);
-TRUNCATE TABLE Example1;
-INSERT INTO Example1 (product_id) VALUES (1);
-INSERT INTO Example1 (product_id) VALUES (3);
-# Test against Example 1:
+# Test of Solution against Example 1:
 CHECKSUM TABLE Example1, Solution;
