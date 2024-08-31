0aa87b0704f9f676a0da7909c8f84b375d54c311
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2024-08-31 19:24:47 +0200

    Solve problem 595. "Big Countries": fix SQL schema

--- problems/big-countries/sql-schema.sql
+++ problems/big-countries/sql-schema.sql
@@ -1,7 +1,9 @@
-Create table If Not Exists World (name varchar(255), continent varchar(255), area int, population int, gdp bigint)
-Truncate table World
-insert into World (name, continent, area, population, gdp) values ('Afghanistan', 'Asia', '652230', '25500100', '20343000000')
-insert into World (name, continent, area, population, gdp) values ('Albania', 'Europe', '28748', '2831741', '12960000000')
-insert into World (name, continent, area, population, gdp) values ('Algeria', 'Africa', '2381741', '37100000', '188681000000')
-insert into World (name, continent, area, population, gdp) values ('Andorra', 'Europe', '468', '78115', '3712000000')
-insert into World (name, continent, area, population, gdp) values ('Angola', 'Africa', '1246700', '20609294', '100990000000')
+-- Table should have been named `Countries`...
+CREATE TABLE IF NOT EXISTS World (name VARCHAR(255), continent VARCHAR(255), area INT, population INT, gdp BIGINT);
+TRUNCATE TABLE World;
+INSERT INTO World (name, continent, area, population, gdp) VALUES
+  ROW('Afghanistan', 'Asia', 652230, 25500100, 20343000000),
+  ROW('Albania', 'Europe', 28748, 2831741, 12960000000),
+  ROW('Algeria', 'Africa', 2381741, 37100000, 188681000000),
+  ROW('Andorra', 'Europe', 468, 78115, 3712000000),
+  ROW('Angola', 'Africa', 1246700, 20609294, 100990000000);
