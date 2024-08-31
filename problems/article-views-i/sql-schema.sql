d24c6729b75ee3f9260539a9e1f86c62645fcf5e
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2024-08-31 19:46:16 +0200

    Solve problem 1148. "Article Views I": fix SQL schema

--- problems/article-views-i/sql-schema.sql
+++ problems/article-views-i/sql-schema.sql
@@ -1,9 +1,10 @@
-Create table If Not Exists Views (article_id int, author_id int, viewer_id int, view_date date)
-Truncate table Views
-insert into Views (article_id, author_id, viewer_id, view_date) values ('1', '3', '5', '2019-08-01')
-insert into Views (article_id, author_id, viewer_id, view_date) values ('1', '3', '6', '2019-08-02')
-insert into Views (article_id, author_id, viewer_id, view_date) values ('2', '7', '7', '2019-08-01')
-insert into Views (article_id, author_id, viewer_id, view_date) values ('2', '7', '6', '2019-08-02')
-insert into Views (article_id, author_id, viewer_id, view_date) values ('4', '7', '1', '2019-07-22')
-insert into Views (article_id, author_id, viewer_id, view_date) values ('3', '4', '4', '2019-07-21')
-insert into Views (article_id, author_id, viewer_id, view_date) values ('3', '4', '4', '2019-07-21')
+CREATE TABLE IF NOT EXISTS Views (article_id INT, author_id INT, viewer_id INT, view_date DATE);
+TRUNCATE TABLE Views;
+INSERT INTO Views (article_id, author_id, viewer_id, view_date) VALUES
+  ROW(1, 3, 5, '2019-08-01'),
+  ROW(1, 3, 6, '2019-08-02'),
+  ROW(2, 7, 7, '2019-08-01'),
+  ROW(2, 7, 6, '2019-08-02'),
+  ROW(4, 7, 1, '2019-07-22'),
+  ROW(3, 4, 4, '2019-07-21'),
+  ROW(3, 4, 4, '2019-07-21');
