366aff41f3056a96fa3261983d8151807271d798
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2024-09-03 00:16:14 +0200

    Solve problem 2634. "Filter Elements from Array": add examples as tests

--- problems/filter-elements-from-array/filterElementsFromArray.js
+++ problems/filter-elements-from-array/filterElementsFromArray.js
@@ -6,3 +6,20 @@
 var filter = function(arr, fn) {
     
 };
+
+const assert = require('node:assert').strict;
+
+const arr1 = [0, 10, 20, 30];
+const greaterThan10 = (n) => n > 10;
+const newArray1 = filter(arr1, greaterThan10);
+assert.deepEqual(newArray1, [20, 30]);
+
+const arr2 = [1, 2, 3];
+const firstIndex = (n, i) => i === 0;
+const newArray2 = filter(arr2, firstIndex);
+assert.deepEqual(newArray2, [1]);
+
+const arr3 = [-2, -1, 0, 1, 2];
+const plusOne = (n) => n + 1;
+const newArray3 = filter(arr3, plusOne);
+assert.deepEqual(newArray3, [-2, 0, 1, 2]);
