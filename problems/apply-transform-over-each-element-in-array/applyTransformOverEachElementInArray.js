34ec864e6535df9d31ccf361234a5aa0a5822adf
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2024-09-02 21:59:10 +0200

    Solve problem 2635. "Apply Transform Over Each Element in Array": add examples as tests

--- problems/apply-transform-over-each-element-in-array/applyTransformOverEachElementInArray.js
+++ problems/apply-transform-over-each-element-in-array/applyTransformOverEachElementInArray.js
@@ -6,3 +6,20 @@
 var map = function(arr, fn) {
     
 };
+
+const assert = require('node:assert').strict;
+
+const arr1 = [1, 2, 3];
+const plusone = (n) => n + 1;
+const newArray1 = map(arr1, plusone);
+assert.deepEqual(newArray1, [2, 3, 4]);
+
+const arr2 = [1, 2, 3];
+const plusI = (n, i) => n + i;
+const newArray2 = map(arr2, plusI);
+assert.deepEqual(newArray2, [1, 3, 5]);
+
+const arr3 = [10, 20, 30];
+const constant = () => 42;
+const newArray3 = map(arr3, constant);
+assert.deepEqual(newArray3, [42, 42, 42]);
