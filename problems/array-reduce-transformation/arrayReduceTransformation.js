faa2cb9ac4fd5523938ba90b717c1d971d8ffd8c
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2024-09-03 00:37:31 +0200

    Solve problem 2626. "Array Reduce Transformation": add examples as tests

--- problems/array-reduce-transformation/arrayReduceTransformation.js
+++ problems/array-reduce-transformation/arrayReduceTransformation.js
@@ -7,3 +7,23 @@
 var reduce = function(nums, fn, init) {
     
 };
+
+const assert = require('node:assert').strict;
+
+const nums1 = [1, 2, 3, 4];
+const sum1 = (accum, curr) => accum + curr;
+const init1 = 0;
+const result1 = reduce(nums1, sum1, init1);
+assert.deepEqual(result1, 10);
+
+const nums2 = [1, 2, 3, 4];
+const sum2 = (accum, curr) => accum + curr * curr;
+const init2 = 100;
+const result2 = reduce(nums2, sum2, init2);
+assert.deepEqual(result2, 130);
+
+const nums3 = [];
+const sum3 = (accum, curr) => 0;
+const init3 = 25;
+const result3 = reduce(nums3, sum3, init3);
+assert.deepEqual(result3, 25);
