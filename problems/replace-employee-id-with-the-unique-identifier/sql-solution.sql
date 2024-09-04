bcbbcf52ff8c2de80f1384f946d72993ee58ea45
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2024-09-05 00:54:46 +0200

    Solve problem 1378. "Replace Employee ID With The Unique Identifier"

--- problems/replace-employee-id-with-the-unique-identifier/sql-solution.sql
+++ problems/replace-employee-id-with-the-unique-identifier/sql-solution.sql
@@ -1,1 +1,8 @@
-# Write your MySQL query statement below
+DROP TABLE IF EXISTS Solution;
+CREATE TABLE Solution AS (
+
+SELECT i.unique_id, e.name
+FROM Employees e
+LEFT JOIN EmployeeUNI i ON i.id = e.id
+
+);
