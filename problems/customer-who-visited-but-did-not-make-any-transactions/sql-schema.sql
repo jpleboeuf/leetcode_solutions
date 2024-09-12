c3468e5857ec0e1653f36050071c0b424b9e7eda
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2024-09-13 01:28:59 +0200

    Solve problem 1581. "Customer Who Visited but Did Not Make Any Transactions": fix SQL schema

--- problems/customer-who-visited-but-did-not-make-any-transactions/sql-schema.sql
+++ problems/customer-who-visited-but-did-not-make-any-transactions/sql-schema.sql
@@ -1,16 +1,18 @@
-Create table If Not Exists Visits(visit_id int, customer_id int)
-Create table If Not Exists Transactions(transaction_id int, visit_id int, amount int)
-Truncate table Visits
-insert into Visits (visit_id, customer_id) values ('1', '23')
-insert into Visits (visit_id, customer_id) values ('2', '9')
-insert into Visits (visit_id, customer_id) values ('4', '30')
-insert into Visits (visit_id, customer_id) values ('5', '54')
-insert into Visits (visit_id, customer_id) values ('6', '96')
-insert into Visits (visit_id, customer_id) values ('7', '54')
-insert into Visits (visit_id, customer_id) values ('8', '54')
-Truncate table Transactions
-insert into Transactions (transaction_id, visit_id, amount) values ('2', '5', '310')
-insert into Transactions (transaction_id, visit_id, amount) values ('3', '5', '300')
-insert into Transactions (transaction_id, visit_id, amount) values ('9', '5', '200')
-insert into Transactions (transaction_id, visit_id, amount) values ('12', '1', '910')
-insert into Transactions (transaction_id, visit_id, amount) values ('13', '2', '970')
+CREATE TABLE IF NOT EXISTS Visits(visit_id INT, customer_id INT);
+CREATE TABLE IF NOT EXISTS Transactions(transaction_id INT, visit_id INT, amount INT);
+TRUNCATE TABLE Visits;
+INSERT INTO Visits (visit_id, customer_id) VALUES
+  ROW(1, 23),
+  ROW(2, 9),
+  ROW(4, 30),
+  ROW(5, 54),
+  ROW(6, 96),
+  ROW(7, 54),
+  ROW(8, 54);
+TRUNCATE TABLE Transactions;
+INSERT INTO Transactions (transaction_id, visit_id, amount) VALUES
+  ROW(2, 5, 310),
+  ROW(3, 5, 300),
+  ROW(9, 5, 200),
+  ROW(12, 1, 910),
+  ROW(13, 2, 970);
