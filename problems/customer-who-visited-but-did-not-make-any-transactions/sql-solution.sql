bc297a915ccaf7842f80ca89814ee0a3b329c04c
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2024-09-13 01:29:14 +0200

    Solve problem 1581. "Customer Who Visited but Did Not Make Any Transactions"

--- problems/customer-who-visited-but-did-not-make-any-transactions/sql-solution.sql
+++ problems/customer-who-visited-but-did-not-make-any-transactions/sql-solution.sql
@@ -1,1 +1,12 @@
-# Write your MySQL query statement below
+DROP TABLE IF EXISTS Solution;
+CREATE TABLE Solution AS (
+
+SELECT v.customer_id, COUNT(v.visit_id) AS count_no_trans
+FROM Visits v
+WHERE v.visit_id NOT IN (
+    SELECT DISTINCT t.visit_id
+    FROM Transactions t
+)
+GROUP BY v.customer_id
+
+);
