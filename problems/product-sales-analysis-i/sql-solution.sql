0f376f21d384aa9f6c8edd91ccfffb8c89a15c44
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2024-09-12 23:19:04 +0200

    Solve problem 1068. "Product Sales Analysis I"

--- problems/product-sales-analysis-i/sql-solution.sql
+++ problems/product-sales-analysis-i/sql-solution.sql
@@ -1,1 +1,8 @@
-# Write your MySQL query statement below
+DROP TABLE IF EXISTS Solution;
+CREATE TABLE Solution AS (
+
+SELECT p.product_name, s.year, s.price
+FROM Sales s
+LEFT JOIN Product p ON p.product_id = s.product_id
+
+);
