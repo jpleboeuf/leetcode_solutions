fbc8053fe70fdf36eeffd1c3feeed52d95cc941a
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2024-09-12 23:18:34 +0200

    Solve problem 1068. "Product Sales Analysis I": fix SQL schema

--- problems/product-sales-analysis-i/sql-schema.sql
+++ problems/product-sales-analysis-i/sql-schema.sql
@@ -1,10 +1,13 @@
-Create table If Not Exists Sales (sale_id int, product_id int, year int, quantity int, price int)
-Create table If Not Exists Product (product_id int, product_name varchar(10))
-Truncate table Sales
-insert into Sales (sale_id, product_id, year, quantity, price) values ('1', '100', '2008', '10', '5000')
-insert into Sales (sale_id, product_id, year, quantity, price) values ('2', '100', '2009', '12', '5000')
-insert into Sales (sale_id, product_id, year, quantity, price) values ('7', '200', '2011', '15', '9000')
-Truncate table Product
-insert into Product (product_id, product_name) values ('100', 'Nokia')
-insert into Product (product_id, product_name) values ('200', 'Apple')
-insert into Product (product_id, product_name) values ('300', 'Samsung')
+CREATE TABLE IF NOT EXISTS Sales (sale_id INT, product_id INT, year INT, quantity INT, price INT);
+-- Table should have been named `Products`...
+CREATE TABLE IF NOT EXISTS Product (product_id INT, product_name VARCHAR(10));
+TRUNCATE TABLE Sales;
+INSERT INTO Sales (sale_id, product_id, year, quantity, price) VALUES
+  ROW(1, 100, 2008, 10, 5000),
+  ROW(2, 100, 2009, 12, 5000),
+  ROW(7, 200, 2011, 15, 9000);
+TRUNCATE TABLE Product;
+INSERT INTO Product (product_id, product_name) VALUES
+  ROW(100, 'Nokia'),
+  ROW(200, 'Apple'),
+  ROW(300, 'Samsung');
