6a66d46bca213f86213d5d45e69839768fa8804a
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2024-09-13 02:45:09 +0200

    Solve problem 197. "Rising Temperature" (wrong)

--- problems/rising-temperature/sql-solution.sql
+++ problems/rising-temperature/sql-solution.sql
@@ -1,1 +1,11 @@
-# Write your MySQL query statement below
+DROP TABLE IF EXISTS Solution;
+CREATE TABLE Solution AS (
+
+SELECT id
+FROM (
+  SELECT w.id, w.temperature, LAG(w.temperature) OVER(ORDER BY w.recordDate) AS prev_temperature
+  FROM Weather w
+) AS temp
+WHERE temperature > prev_temperature
+
+);
