e1b36e180683ee16ac710cdb56e7ee854661d8f0
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2024-09-13 02:44:40 +0200

    Solve problem 197. "Rising Temperature": fix SQL schema

--- problems/rising-temperature/sql-schema.sql
+++ problems/rising-temperature/sql-schema.sql
@@ -1,6 +1,8 @@
-Create table If Not Exists Weather (id int, recordDate date, temperature int)
-Truncate table Weather
-insert into Weather (id, recordDate, temperature) values ('1', '2015-01-01', '10')
-insert into Weather (id, recordDate, temperature) values ('2', '2015-01-02', '25')
-insert into Weather (id, recordDate, temperature) values ('3', '2015-01-03', '20')
-insert into Weather (id, recordDate, temperature) values ('4', '2015-01-04', '30')
+-- Table should have been named `Temperatures`...
+CREATE TABLE IF NOT EXISTS Weather (id INT, recordDate DATE, temperature INT);
+TRUNCATE TABLE Weather;
+INSERT INTO Weather (id, recordDate, temperature) VALUES
+  ROW(1, '2015-01-01', 10),
+  ROW(2, '2015-01-02', 25),
+  ROW(3, '2015-01-03', 20),
+  ROW(4, '2015-01-04', 30);
