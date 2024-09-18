232dcb9bd759f9241e21045709a51b6abe43fdde
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2024-09-18 10:11:43 +0200

    Solve problem 577. "Employee Bonus": fix SQL schema

--- problems/employee-bonus/sql-schema.sql
+++ problems/employee-bonus/sql-schema.sql
@@ -1,10 +1,14 @@
-Create table If Not Exists Employee (empId int, name varchar(255), supervisor int, salary int)
-Create table If Not Exists Bonus (empId int, bonus int)
-Truncate table Employee
-insert into Employee (empId, name, supervisor, salary) values ('3', 'Brad', NULL, '4000')
-insert into Employee (empId, name, supervisor, salary) values ('1', 'John', '3', '1000')
-insert into Employee (empId, name, supervisor, salary) values ('2', 'Dan', '3', '2000')
-insert into Employee (empId, name, supervisor, salary) values ('4', 'Thomas', '3', '4000')
-Truncate table Bonus
-insert into Bonus (empId, bonus) values ('2', '500')
-insert into Bonus (empId, bonus) values ('4', '2000')
+-- Table should have been named `Employees`...
+CREATE TABLE IF NOT EXISTS Employee (empId INT, name VARCHAR(255), supervisor INT, salary INT);
+-- Table should have been named `Bonuses`...
+CREATE TABLE IF NOT EXISTS Bonus (empId INT, bonus INT);
+TRUNCATE TABLE Employee;
+INSERT INTO Employee (empId, name, supervisor, salary) VALUES
+  ROW(3, 'Brad',   NULL, 4000),
+  ROW(1, 'John',   3,    1000),
+  ROW(2, 'Dan',    3,    2000),
+  ROW(4, 'Thomas', 3,    4000);
+TRUNCATE TABLE Bonus;
+INSERT INTO Bonus (empId, bonus) VALUES
+  ROW(2,  500),
+  ROW(4, 2000);
