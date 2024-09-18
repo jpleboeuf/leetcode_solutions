cd6e4af427a41aa66979a0a8bd2d14f561dae3aa
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2024-09-18 13:56:26 +0200

    Solve problem 1280. "Students and Examinations"

--- problems/students-and-examinations/sql-solution.sql
+++ problems/students-and-examinations/sql-solution.sql
@@ -1,1 +1,20 @@
-# Write your MySQL query statement below
+DROP TABLE IF EXISTS Solution;
+CREATE TABLE Solution AS (
+
+WITH
+    students_subjects_prod AS (
+        SELECT s.student_id, s.student_name, d.subject_name
+        FROM Students s, Subjects d
+    ),
+    exam_count AS (
+        SELECT e.student_id, e.subject_name, COUNT(*) AS attended_exams
+        FROM Examinations e
+        GROUP BY e.student_id, e.subject_name
+    )
+SELECT ssp.student_id, ssp.student_name, ssp.subject_name,
+    IFNULL(ec.attended_exams, 0) AS attended_exams
+FROM students_subjects_prod ssp
+LEFT JOIN exam_count ec USING(student_id, subject_name)
+ORDER BY ssp.student_id, ssp.subject_name
+
+);
